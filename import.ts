2b318dcfd5d68de9e161602813e6f01ba6653281
Benjamin Shutt <shuttben@gmail.com>
2021-04-17 23:08:54 -0700

    Changed import to first download  latest version of itself, and then relaunch itself. This allow it to download new files added to the file list without needed to run it again manually.

--- import.ts
+++ import.ts
@@ -7,34 +7,64 @@ let config = {
     serverPrefix: "PhaZ",
 }
 
+let importFilename = "import.js";
+
+let filenames = [
+    "autohack-target.js",
+    //"purchase-hacknet-node.js",
+    "purchase-server-8gb.js",
+    "utilities.js",
+];
+
 export async function main(ns: NS) {
-    let filesImported = await importFiles(ns);
-    if (filesImported) {
-        ns.tprint("Download successful!");
-        ns.tprint(`Files have been downloaded into ${config.folder} directory`);
+    let secondStage = false;
+    if (ns.args.length == 1 && typeof ns.args[0] === "boolean") {
+        secondStage = ns.args[0];
+    }
+
+    if (!secondStage) {
+        let fileImported = await importImport(ns);
+        if (fileImported) {
+            ns.spawn(importFilename, 1, true);
+        }
+        else {
+            ns.tprint("Download failed!");
+        }
     }
     else {
-        ns.tprint("Download failed!");
+        let filesImported = await importFiles(ns);
+        if (filesImported) {
+            ns.tprint("Download successful!");
+            ns.tprint(`Files have been downloaded into ${config.folder} directory`);
+        }
+        else {
+            ns.tprint("Download failed!");
+        }
     }
 }
 
 async function importFiles(ns: NS) {
-    let files = [
-        "autohack-target.js",
-        "import.js",
-        "purchase-server-8gb.js",
-        "utilities.js",
-    ];
     let filesImported = true;
 
-    for (let file of files) {
-        let remoteFileName = `${config.rootUrl}scripts/${file}`;
-        let result = await ns.wget(remoteFileName, `/${config.folder}/${file}`);
+    for (let filename of filenames) {
+        let result = await importFile(ns, filename);
         filesImported = filesImported && result;
-        ns.tprint(`File: ${file}: ${result ? '✔' : '✖'}`);
+        ns.tprint(`File: ${filename}: ${result ? '✔' : '✖'}`);
     }
 
     return filesImported;
+}
+
+async function importImport(ns: NS) {
+    let result = await importFile(ns, importFilename)
+    ns.tprint(`File: ${importFilename}: ${result ? '✔' : '✖'}`);
+    return result;
+}
+
+async function importFile(ns: NS, filename) {
+    let remoteFileName = `${config.rootUrl}scripts/${filename}`;
+    let result = await ns.wget(remoteFileName, `/${config.folder}/${filename}`);
+    return result;
 }
 
 export function getServerPrefix() {
